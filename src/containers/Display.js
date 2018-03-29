cf2ddb7dffd530a66686b5b0467e954a47be6631
Nicholas Weitzel <nicholasweitzel@gmail.com>
2018-03-29 03:29:18 -0400

    Add Login component. Not fully implemented.

--- src/containers/Display.js
+++ src/containers/Display.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { Login } from './components/Login';
 import './Containers.css';
 
 export const Display = () => (
   <section className="display">
     empty display
+    <Login />
   </section>
 );
 
