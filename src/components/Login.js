0bfbcc44f742108db1198dd4e267cd9af89e0a23
Nicholas Weitzel <nicholasweitzel@gmail.com>
2018-03-29 22:52:40 -0400

    Make successful guest login and coinlist requests to heroku backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-import { backendUrl, loginRoute /* , coinListRoute */ } from '../constants';
+import { backendUrl, loginRoute, coinListRoute } from '../constants';
 
 export class Login extends React.Component {
   constructor(props) {
@@ -14,7 +14,7 @@ export class Login extends React.Component {
   handleClick() {
     let newId;
 
-    /* axios.get(backendUrl + coinListRoute)
+    axios.get(backendUrl + coinListRoute)
       .then((response) => {
         // eslint-disable-next-line no-console
         console.log(`coin list response properties: ${Object.keys(response)}`);
@@ -32,26 +32,21 @@ export class Login extends React.Component {
         console.log(`coin list response.request: ${response.request}`);
         // eslint-disable-next-line no-console
         console.log(`coin list response.data[0] properties: ${Object.keys(response.data[0])}`);
-
-        response.data.forEach((coin) => {
-          // eslint-disable-next-line no-console
-          console.log(`id: ${coin.id} symbol: ${coin.symbol}  name: ${coin.name}`);
-        });
+        // eslint-disable-next-line no-console
+        console.log(`id: ${response.data[0].id} symbol: ${response.data[0].symbol}  name: ${response.data[0].name}`);
       })
       .catch((error) => {
         // eslint-disable-next-line no-console
         console.log(`coin list error: ${error.response}`);
-      }); */
+      });
 
-    axios.post(backendUrl + loginRoute, {
-      username: 'guest',
-      password: 'blahblah',
+    /* fetch(backendUrl + loginRoute, {
+      method: 'post',
+      body: JSON.stringify({ username: 'guest', password: 'blahblah' }),
     })
       .then((response) => {
         // eslint-disable-next-line no-console
-        console.log(`login response is: ${response}`);
-        // eslint-disable-next-line no-console
-        console.log(`login response properties: ${Object.keys(response)}`);
+        console.log(`fetch login response properties are: ${Object.keys(response)}`);
 
         newId = response;
 
@@ -69,8 +64,45 @@ export class Login extends React.Component {
       })
       .catch((error) => {
         // eslint-disable-next-line no-console
+        console.log(`fetch login error properties are: ${Object.keys(error)}`);
+      }); // end fetch post() */
+
+    axios.post(backendUrl + loginRoute, {
+      username: 'guest',
+      password: 'blahblah',
+    })
+      .then((response) => {
+        // eslint-disable-next-line no-console
+        console.log(`login response is: ${response}`);
+        // eslint-disable-next-line no-console
+        console.log(`login response properties: ${Object.keys(response)}`);
+        // eslint-disable-next-line no-console
+        console.log(`login response data properties: ${Object.keys(response.data)}`);
+        // eslint-disable-next-line no-console
+        console.log(`login response headers properties: ${Object.keys(response.headers)}`);
+        // eslint-disable-next-line no-console
+        console.log(`login response config properties: ${Object.keys(response.config)}`);
+        // eslint-disable-next-line no-console
+        console.log(`login response request properties: ${Object.keys(response.request)}`);
+
+        newId = response.data.userId;
+
+        this.setState({
+          id: newId,
+        }); // end setState()
+
+        if (this.state.id !== null) {
+          // eslint-disable-next-line no-console
+          console.log(`Got ID!!!!: ${this.state.id}`);
+        } else {
+          // eslint-disable-next-line no-console
+          console.log('id still null');
+        } // end if/else
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
         console.log(`login error is: ${error.response}`);
-      }); // end post()
+      }); // end axios.post()
   } // end handleClick()
 
   render() {
