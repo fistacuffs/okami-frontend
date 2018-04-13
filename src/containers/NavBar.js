7e8c4285616cb97bd5bdfe8c0d1ded0e27a3557d
James <5463611+Cakedonut@users.noreply.github.com>
2018-04-13 15:16:16 -0800

    Work on adding reactstrap for bootstrap theming

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
 import './Containers.css';
 
 export const NavBar = () => (
-  <nav className="nav-bar">
-    empty navBar
-  </nav>
+  <Navbar color="dark" dark toggleable>
+    <NavbarBrand href="/">Okami</NavbarBrand>
+    <NavbarToggler right />
+    <Collapse isOpen={false} navbar>
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <NavLink href="/components/">Components</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+        </NavItem>
+      </Nav>
+    </Collapse>
+  </Navbar>
 );
 
 export default NavBar;
