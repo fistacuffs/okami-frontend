c167ab8a26815f9524ec6823702e830aeb603459
James <5463611+Cakedonut@users.noreply.github.com>
2018-04-13 17:00:50 -0800

    Preliminary work on matching components to website mock-up

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,31 +1,40 @@
 import React from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem, Button } from 'reactstrap';
 
 import './Containers.css';
 
-export const NavBar = () => (
-  <Navbar color="dark" dark toggleable>
-    <NavbarBrand href="/">Okami</NavbarBrand>
-    <NavbarToggler right />
-    <Collapse isOpen={false} navbar>
-      <Nav className="ml-auto" navbar>
-        <NavItem>
-          <NavLink href="/components/">Components</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-        </NavItem>
-      </Nav>
-    </Collapse>
-  </Navbar>
-);
+export class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+  render() {
+    return (
+      <Navbar color="dark" dark className="navbar" toggleable>
+        <NavbarToggler onClick={this.toggle} left />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <Button color="primary" className="navbar-button">Sign Up</Button>
+            </NavItem>
+            <NavItem>
+              <Button color="primary" className="navbar-button">Login</Button>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
+}
 
 export default NavBar;
+
